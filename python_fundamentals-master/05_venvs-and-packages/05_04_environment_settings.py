cf7d68c58b0cf75c6fcb314c41df839221cc99cd
Martin <martinh500@gmail.com>
2021-06-21 18:18:19 +0200

    venv commit

---
+++ python_fundamentals-master/05_venvs-and-packages/05_04_environment_settings.py
@@ -0,0 +1,16 @@
+# Create a virtual environment and edit the activation script to add
+# the following information:
+# 
+# - ENVIRONMENT="development"
+# - SECRET="i ate your sweets"
+# 
+# Then write the necessary code to access and print the values of these
+# two environment variables in this script.
+
+import pendulum
+import os
+
+environment = os.environ['ENVIRONMENT']
+secret = os.environ['SECRET']
+print(secret)
+print(environment) 
